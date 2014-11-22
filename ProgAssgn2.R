b4f53d759250f2798d50b460fe7c4c4c7ce3799a
data_kiin <kamran.haroon@outlook.com>
2014-11-22 15:32:21 +0500

    initial commit without comments

---
+++ ProgAssgn2.R
@@ -0,0 +1,28 @@
+# Programming assignment 2
+
+# Example: Caching the mean of a vector
+
+makeVector <- function(x = numeric()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setMean <- function(mean) m <<- mean
+      getMean <- function(mean) m
+      list(set = set, get = get, setMean = setMean, getMean = getMean)
+}
+
+
+cacheMean <- function(x, ...) {
+      m <- x$getMean()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- mean(data, ...)
+      x$setMean(m)
+      m
+}
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2
+## Functions to cache the inverse of an invertible matrix
 
-## Write a short comment describing this function
+## This function creates a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      s <- NULL
+      set <- function(y) {
+            x <<- y
+            s <<- NULL
+      }
+      get <- function() x
+      setSolve <- function(solve) s <<- solve
+      getSolve <- function(solve) s
+      list(set = set, get = get, setSolve = setSolve, getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes and returns the inverse of the special matrix object
+## created by the makeCacheMatrix() function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      s <- x$getSolve()
+      if (!is.null(s)) {
+            message("getting cached data")
+            return(s)
+      }
+      data <- x$get()
+      s <- solve(data, ...)
+      x$setSolve(s)
+      s
 }
+
+
+## Test cases
